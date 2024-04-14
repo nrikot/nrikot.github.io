d38498f4af2e850b550053501102dcbfe084c63a
N Riko Trihendrawan <riko.trihendrawan@gmail.com>
2024-04-14 18:10:12 +0700

    Updated Wordpress portfolio

---
+++ portfolio/mern-places/precache-manifest.3e61f8b389885650812caa4f29085cce.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1cc7f9f3e49841b6b88c41a7fb5fef",
+    "url": "/portfolio/mern-places/index.html"
+  },
+  {
+    "revision": "ca38234d77810985079c",
+    "url": "/portfolio/mern-places/static/css/3.52153eb1.chunk.css"
+  },
+  {
+    "revision": "89e5a1cf7e4400291696",
+    "url": "/portfolio/mern-places/static/css/4.64b7851f.chunk.css"
+  },
+  {
+    "revision": "dbfbd90c9a9fa867bbee",
+    "url": "/portfolio/mern-places/static/css/5.f7e2ce2a.chunk.css"
+  },
+  {
+    "revision": "309d6ef73acac671ca29",
+    "url": "/portfolio/mern-places/static/css/6.200f08ba.chunk.css"
+  },
+  {
+    "revision": "bae9f1e5c1afd2864a21",
+    "url": "/portfolio/mern-places/static/css/7.7ff8b807.chunk.css"
+  },
+  {
+    "revision": "b6cc3133b02934fb6a69",
+    "url": "/portfolio/mern-places/static/css/main.96dfa7a3.chunk.css"
+  },
+  {
+    "revision": "a4a555ee8a8bf2a76bf6",
+    "url": "/portfolio/mern-places/static/js/2.665d46fa.chunk.js"
+  },
+  {
+    "revision": "ca38234d77810985079c",
+    "url": "/portfolio/mern-places/static/js/3.1570fbc9.chunk.js"
+  },
+  {
+    "revision": "89e5a1cf7e4400291696",
+    "url": "/portfolio/mern-places/static/js/4.7b367f90.chunk.js"
+  },
+  {
+    "revision": "dbfbd90c9a9fa867bbee",
+    "url": "/portfolio/mern-places/static/js/5.3b38123d.chunk.js"
+  },
+  {
+    "revision": "309d6ef73acac671ca29",
+    "url": "/portfolio/mern-places/static/js/6.269935d9.chunk.js"
+  },
+  {
+    "revision": "bae9f1e5c1afd2864a21",
+    "url": "/portfolio/mern-places/static/js/7.acace280.chunk.js"
+  },
+  {
+    "revision": "34100116150db92c7c61",
+    "url": "/portfolio/mern-places/static/js/8.4f149b55.chunk.js"
+  },
+  {
+    "revision": "b6cc3133b02934fb6a69",
+    "url": "/portfolio/mern-places/static/js/main.b191e9c7.chunk.js"
+  },
+  {
+    "revision": "39292a5649bab90e328a",
+    "url": "/portfolio/mern-places/static/js/runtime-main.a5fefc44.js"
+  },
+  {
+    "revision": "c08efe7bc0e4c5993e308e4f94be7e78",
+    "url": "/portfolio/mern-places/static/media/places_logo.c08efe7b.svg"
+  }
+]);
